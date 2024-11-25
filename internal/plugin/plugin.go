a7ade98b0c939bcb6a408a7d57852a52145afd18
Stanislavs Jurgensons <stanislavs.jurgensons@zabbix.com>
2024-11-25 17:15:21 +0200

    ...G...... [ZBXNEXT-9042] added licence information

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -1,6 +1,18 @@
 /*
-TODO: Add Apache-2.0 licence
-*/
+**   Copyright 2001-2024 Zabbix SIA
+**
+**   Licensed under the Apache License, Version 2.0 (the "License");
+**   you may not use this file except in compliance with the License.
+**   You may obtain a copy of the License at
+**
+**       http://www.apache.org/licenses/LICENSE-2.0
+**
+**   Unless required by applicable law or agreed to in writing, software
+**   distributed under the License is distributed on an "AS IS" BASIS,
+**   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**   See the License for the specific language governing permissions and
+**   limitations under the License.
+**/
 
 package plugin
 
@@ -79,18 +91,16 @@ func (p *nvmlPlugin) Start() {
 
 	// Try to initialize NVML using InitV2, fallback to Init if it fails
 	err := p.nvmlRunner.InitV2()
-	if err == nil {
-		return
-	}
-
-	p.Logger.Debugf("failed to init runner with InitNVMLv2: %s", err.Error())
-
-	// Fallback to Init if InitV2 fails
-	err = p.nvmlRunner.Init()
 	if err != nil {
-		wrappedErr := errs.Wrap(err, "failed to init NVML library")
-		p.Logger.Errf("%s", wrappedErr.Error())
-		panic(wrappedErr)
+		p.Logger.Debugf("failed to init runner with InitNVMLv2: %s", err.Error())
+
+		// Fallback to Init if InitV2 fails
+		err = p.nvmlRunner.Init()
+		if err != nil {
+			wrappedErr := errs.Wrap(err, "failed to init NVML library")
+			p.Logger.Errf("%s", wrappedErr.Error())
+			panic(wrappedErr)
+		}
 	}
 }
 
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 /*
-TODO: Add Apache-2.0 licence
-*/
+**   Copyright 2001-2024 Zabbix SIA
+**
+**   Licensed under the Apache License, Version 2.0 (the "License");
+**   you may not use this file except in compliance with the License.
+**   You may obtain a copy of the License at
+**
+**       http://www.apache.org/licenses/LICENSE-2.0
+**
+**   Unless required by applicable law or agreed to in writing, software
+**   distributed under the License is distributed on an "AS IS" BASIS,
+**   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**   See the License for the specific language governing permissions and
+**   limitations under the License.
+**/
 
 package main
 
@@ -14,12 +26,21 @@ import (
 	"golang.zabbix.com/sdk/zbxerr"
 )
 
-/*
-TODO: Add Apache-2.0 licence
-*/
 const copyrightMessage = //
 `
-Copyright 2001-%d Zabbix SIA
+   Copyright 2001-%d Zabbix SIA
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 `
 
 //nolint:gochecknoglobals,revive // required ALL_CAPS by build scripts
